ccf41d1e6f883b72073db56759cbc9f0ffca5536
MINDHELL <Tgpurpose640@gmail.com>
2025-09-25 11:43:27 +0530

    Update ping_test.py

--- plugins/ping_test.py
+++ plugins/ping_test.py
@@ -1,6 +1,35 @@
+# plugins/subscription.py
+
 from pyrogram import filters
-from pyrogram.types import Message
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.enums import ParseMode
 from bot import Bot
+from config import *
+
+# Command to show subscription plans
+@Bot.on_message(filters.command('plans') & filters.private)
+async def show_plans(bot: Bot, message):
+    plans_text = PAYMENT_TEXT
+    buttons = InlineKeyboardMarkup([
+        [InlineKeyboardButton("Pay via UPI", callback_data="upi_info")],
+        [InlineKeyboardButton("Contact Support", url=f"https://t.me/{OWNER}")]
+    ])
+    await message.reply(plans_text, reply_markup=buttons, parse_mode=ParseMode.HTML)
+
+
+# Command to show UPI payment QR code and instructions
+@Bot.on_message(filters.command('upi') & filters.private)
+async def upi_info(bot: Bot, message):
+    await bot.send_photo(
+        chat_id=message.chat.id,
+        photo=PAYMENT_QR,
+        caption=PAYMENT_TEXT,
+        parse_mode=ParseMode.HTML,
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("Contact Owner", url=f"https://t.me/{OWNER}")]]
+        )
+                                  )
+
 
 @Bot.on_message(filters.command("ping") & filters.private)
 async def ping_test(client, message: Message):
