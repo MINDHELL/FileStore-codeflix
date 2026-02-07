dd2d71588212cd414a4efd80e1e1d9e08a76c4f2
MINDHELL <Tgpurpose640@gmail.com>
2026-02-07 12:42:17 +0530

    Update auto_post_old.py

--- plugins/auto_post_old.py
+++ plugins/auto_post_old.py
@@ -5,7 +5,8 @@ from pyrogram import filters
 from bot import Bot
 from helper_func import encode
 from config import SOURCE_CHANNEL, TARGET_CHANNEL, AUTO_POST_DELAY, OWNER_ID
-from database import db
+from database.database import *
+
 
 BATCH_SIZE = 20
 STOP_FLAG = False
