805110aef5861fb331b004e11f247044a3f5d819
MINDHELL <Tgpurpose640@gmail.com>
2026-02-13 16:15:31 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -27,11 +27,11 @@ def new_user(id):
     return {
         '_id': id,
         'verify_status': {
-            'is_verified': False,
-            'verified_time': 0,
-            'verify_token': "",
-            'link': ""
-            'token_created_at': 0
+    'is_verified': False,
+    'verified_time': 0,
+    'verify_token': "",
+    'link': "",
+    'token_created_at': 0
         }
     }
 
