6d8f48bb5220e01ea6eb300cc0cdb11056d1cf98
MINDHELL <Tgpurpose640@gmail.com>
2026-05-08 20:31:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ API_HASH = os.environ.get("API_HASH", "37895f967d284f6781f99e9beef21ebf") #Your 
 
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003806568021")) #Your db channel Id
 OWNER = os.environ.get("OWNER", "Xsupprt4bot") # Owner username without @
-OWNER_ID = int(os.environ.get("OWNER_ID", "7437503888")) # Owner id
+OWNER_ID = int(os.environ.get("OWNER_ID", "8635942785")) # Owner id
 #--------------------------------------------
 PORT = os.environ.get("PORT", "8001")
 SOURCE_CHANNEL = -1003940157421   # your source channel ID
